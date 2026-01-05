371678c543e83f8f66fa588e9da53222c4898ad8
Rab Miháj <rabmisi1010@gmail.com>
2026-01-05 12:56:12 +0100

    created basic models

---
+++ Library/Models/Author.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public DateTime BirthDate { get; set; }
+        public ICollection<Book> Books = new List<Book>();
+    }
+}
---
+++ Library/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        public int PublicationYear { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
